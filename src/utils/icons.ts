eb429adbd1b12f66bd248fbf8206a8bacd37198d
Blinor Kajtazi <kajtaziblinor@gmail.com>
2024-09-29 18:09:00 +0200

    added icons library

---
+++ src/utils/icons.ts
@@ -0,0 +1,6 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPen, faTrash);
+
+export default library;
